3ca34c59d8cbd0bd379b915c0e3fa168f1c9f6d5
rodzewich <rodzewich.oleg@gmail.com>
2015-08-11 11:53:17 +0300

    -

--- xlib/utils/module/Module.ts
+++ xlib/utils/module/Module.ts
@@ -1,4 +1,3 @@
-/// <reference path="./IModule.ts" />
 /// <reference path="./loader/ILoader.ts" />
 /// <reference path="../../core.ts" />
 /// <reference path="../../node.d.ts" />
@@ -8,36 +7,34 @@ import IModule = require("./IModule");
 import ILoader = require("./loader/ILoader");
 import core = require("../../core");
 
-class Module implements IModule {
+var cwd:string,
+    modules:any = {},
+    loader:ILoader;
 
-    protected _cwd:string;
+class Module {
 
-    protected _modules:any = {};
-
-    protected _loader:ILoader;
-
-    public getCwd():string {
-        return this._cwd;
+    public static getCwd():string {
+        return cwd;
     }
 
-    public setCwd(value:string):void {
-        this._cwd = value;
+    public static setCwd(value:string):void {
+        cwd = value;
     }
 
-    public resetCwd():void {
-        this._cwd = undefined;
+    public static resetCwd():void {
+        cwd = undefined;
     }
 
-    public setLoader(value:ILoader):void {
-        this._loader = value;
+    public static setLoader(value:ILoader):void {
+        loader = value;
     }
 
-    public getLoader():ILoader {
-        return this._loader;
+    public static getLoader():ILoader {
+        return loader;
     }
 
-    public load(module:string, dependency:string[], callback:(error?:Error, exports?:any) => void):void {
-        this.getLoader().load(module, function (error:Error) {
+    public static load(module:string, dependency:string[], callback:(error?:Error, exports?:any) => void):void {
+        Module.getLoader().load(module, function (error:Error) {
 
         });
     }
@@ -50,19 +47,19 @@ class Module implements IModule {
         if (/\s/.test(temp)) {
             throw new Error("bla bla bla");
         }
-        if (core.typeOf(this._modules[temp]) !== "undefined") {
+        if (core.typeOf(Module._modules[temp]) !== "undefined") {
             throw new Error("bla bla bla");
         }
         if (["object", "function"].indexOf(core.typeOf(exports)) !== -1) {
-            this._modules[temp] = exports;
+            Module._modules[temp] = exports;
         } else {
-            this._modules[temp] = {};
+            Module._modules[temp] = {};
         }
     }
 
-    public require(module:string):any {
+    public static require(module:string):any {
         var temp:string = String(module || ""),
-            cwd:string[] = String(this.getCwd()).split("/"),
+            cwd:string[] = String(Module.getCwd()).split("/"),
             path:string[] = temp.split("/"),
             element:string;
         if (!temp) {
@@ -71,10 +68,10 @@ class Module implements IModule {
         if (/\s/.test(temp)) {
             throw new Error("bla bla bla");
         }
-        if (!this.getCwd()) {
+        if (!Module.getCwd()) {
             throw new Error("bla bla bla");
         }
-        if (core.typeOf(this._modules[temp]) === "undefined") {
+        if (core.typeOf(Module._modules[temp]) === "undefined") {
             throw new Error("Module " + JSON.stringify(temp) + " not found");
         }
         if (path.length > 1 && path[0] === "") {
@@ -96,10 +93,10 @@ class Module implements IModule {
                 cwd.push(element);
             }
         }
-        if (cwd.join("/") === String(this.getCwd())) {
+        if (cwd.join("/") === String(Module.getCwd())) {
             throw new Error("bla bla bla");
         }
-        return this._modules[cwd.join("/")];
+        return Module._modules[cwd.join("/")];
     }
 
 }
