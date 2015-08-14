971bf93a6f31973586d80a152965e7d1200adb00
rodzewich <rodzewich.oleg@gmail.com>
2015-08-14 15:30:01 +0300

    -

--- lib/memory/initialization.ts
+++ lib/memory/initialization.ts
@@ -4,7 +4,7 @@ import cp = require("child_process");
 import path = require("path");
 
 function init(location:string, callback:() => void):void {
-    var command:cp.ChildProcess = cp.spawn(process.execPath, [path.join(__dirname, "daemon.js"), location]);
+    var command:cp.ChildProcess = cp.spawn(process.execPath, [path.join(__dirname, "daemon.js"), "--location", location]);
     command.stderr.addListener("data", function (data) {
         console.log(("Memory socket say:\n" + data.toString("utf8")));
     });
