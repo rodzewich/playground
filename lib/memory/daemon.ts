baa4e0cae9f0052f24959d0b680bc4d3a2e12620
oleg.rodzewich <rodzewich.oleg@gmail.com>
2015-08-15 11:52:46 +0300

    -

--- lib/memory/daemon.ts
+++ lib/memory/daemon.ts
@@ -1,11 +1,14 @@
 /// <reference path="../daemon/IDaemon.ts" />
 /// <reference path="../daemon/Daemon.ts" />
-/// <reference path="../node.d.ts" />
+/// <reference path="../../types/" />
 /// <reference path="../optimist.d.ts" />
+/// <reference path="../log4js.d.ts" />
 
 import IDaemon = require("./daemon/IDaemon");
 import Daemon = require("./daemon/Daemon");
 import optimist = require("optimist");
+import log4js = require("log4js");
+var logger:log4js.Logger = log4js.getLogger("daemon");
 
 var argv:any = require('optimist').
         usage('Usage: daemon -l [filename]').
@@ -18,5 +21,5 @@ var argv:any = require('optimist').
     });
 
 daemon.start((error:Error):void => {
-    // todo: log info
+    logger.info("Memory daemon started.");
 });
