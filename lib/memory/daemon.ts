b797da446621941967a91a71f5417519148166a9
rodzewich <rodzewich.oleg@gmail.com>
2015-08-17 12:12:03 +0300

    -

--- lib/memory/daemon.ts
+++ lib/memory/daemon.ts
@@ -1,14 +1,16 @@
-/// <reference path="../daemon/IDaemon.ts" />
-/// <reference path="../daemon/Daemon.ts" />
+/// <reference path="./daemon/IDaemon.ts" />
+/// <reference path="./daemon/Daemon.ts" />
+/// <reference path="../CommonError.ts" />
 /// <reference path="../../types/node/node.d.ts" />
 /// <reference path="../../types/optimist/optimist.d.ts" />
 /// <reference path="../../types/log4js/log4js.d.ts" />
 /// <reference path="../../logger" />
 
-import IDaemon = require("./daemon/IDaemon");
-import Daemon = require("./daemon/Daemon");
-import optimist = require("optimist");
-import log4js = require("../../logger");
+import optimist    = require("optimist");
+import IDaemon     = require("./daemon/IDaemon");
+import Daemon      = require("./daemon/Daemon");
+import log4js      = require("../../logger");
+import CommonError = require("../CommonError");
 var logger:log4js.Logger = log4js.getLogger("worker");
 
 var argv:any = require('optimist').
@@ -24,13 +26,14 @@ var argv:any = require('optimist').
 daemon.start((error:Error):void => {
     if (error) {
         process.stderr.write(JSON.stringify({
-            started: false
-        }));
+            started: false,
+                error: CommonError
+        })+ "\n");
         logger.fatal("Something went wrong", error);
     } else {
         process.stderr.write(JSON.stringify({
             started: true
-        }));
+        })+ "\n");
         logger.info("Memory daemon started");
     }
 });
--- lib/memory/initialization.ts
+++ lib/memory/initialization.ts
@@ -1,30 +1,54 @@
 /// <reference path="../../types/node/node.d.ts" />
 /// <reference path="../../types/log4js/log4js.d.ts" />
 
-import cp     = require("child_process");
-import path   = require("path");
-import log4js = require("log4js");
+import cp                = require("child_process");
+import path              = require("path");
+import log4js            = require("log4js");
+import CommonError       = require("../CommonError");
 var logger:log4js.Logger = log4js.getLogger("client");
 
 function init(location:string, callback:(error?:Error) => void):void {
     var daemon:string = path.join(__dirname, "daemon.js"),
         command:cp.ChildProcess = cp.spawn(process.execPath, [daemon, "--location", location]),
+        response: Buffer = new Buffer(0),
+        echo:(stream:NodeJS.WritableStream, data:Buffer) => void = (stream:NodeJS.WritableStream, data:Buffer):void => {
+            stream.write(data);
+        },
         handler:(data:Buffer) => void = (data:Buffer):void => {
-            console.log(data.toString("utf8"));
             var result:any,
-                error:Error;
-            try {
-                result = JSON.parse(data.toString("utf8")) || {started: false};
-            } catch (err) {
-                result = {started: false};
-            }
-            if (!result.started) {
-                error = new Error("bla bla bla"); // todo: use error from stdout
-                logger.fatal("Something went wrong", error);
-                callback(error);
-            } else {
-                logger.info("Memory daemon started");
-                callback(null);
+                error:Error,
+                string: string,
+                index: number,
+                json: string;
+            response = Buffer.concat([response, data]);
+            string = data.toString("utf8");
+            index = string.indexOf("\n");
+            if (index !== -1) {
+                command.stderr.removeListener("data", handler);
+                command.stdout.removeListener("data", handler);
+                command.stderr.addListener("data", (data:Buffer):void => {
+                    echo(process.stderr, data);
+                });
+                command.stdout.addListener("data", (data:Buffer):void => {
+                    echo(process.stdout, data);
+                });
+                json = string.slice(0, index + 1);
+                try {
+                    result = JSON.parse(json) || {started: false, error: {message: "Unknown error"}};
+                } catch (err) {
+                    logger.warn("Worker send error content", response.toString("utf8"));
+                    process.stderr.write(response);
+                    result = {started: false, error: err};
+                }
+                response = response.slice((new Buffer(json, "utf8")).length + 1);
+                if (!result.started) {
+                    error = new CommonError(result.error);
+                    logger.fatal("Something went wrong", error);
+                    callback(error);
+                } else {
+                    logger.info("Memory daemon started");
+                    callback(null);
+                }
             }
         };
     command.stderr.addListener("data", handler);
