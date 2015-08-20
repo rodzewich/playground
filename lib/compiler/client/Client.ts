f2ed64bbfcb5d008627b96eaad5aee35adfc0788
rodzewich <rodzewich.oleg@gmail.com>
2015-08-20 10:59:58 +0300

    -

--- lib/compiler/client/Client.ts
+++ lib/compiler/client/Client.ts
@@ -4,6 +4,7 @@
 /// <reference path="../../../types/node/node.d.ts" />
 /// <reference path="../../deferred.ts" />
 
+import typeOf = require("../../typeOf");
 import AbstractClient = require("../../client/Client");
 import IOptions = require("./IOptions");
 import IClient = require("./IClient");
@@ -28,59 +29,60 @@ class Client extends AbstractClient implements IClient {
     }
 
     public connect(callback:(errors?:Error[]) => void): void {
-        var command:cp.ChildProcess,
-            response: Buffer = new Buffer(0),
-            echo:(stream:NodeJS.WritableStream, data:Buffer) => void =
-                (stream:NodeJS.WritableStream, data:Buffer):void => {
-                    stream.write(data);
-                },
-            handler:(data:Buffer) => void = (data:Buffer):void => {
-                var result:any,
-                    error:Error,
-                    string: string,
-                    index: number,
-                    json: string;
-                response = Buffer.concat([response, data]);
-                string = data.toString("utf8");
-                index = string.indexOf("\n");
-                if (index !== -1) {
-                    command.stderr.removeListener("data", handler);
-                    command.stdout.removeListener("data", handler);
-                    command.stderr.addListener("data", (data:Buffer):void => {
-                        echo(process.stderr, data);
-                    });
-                    command.stdout.addListener("data", (data:Buffer):void => {
-                        echo(process.stdout, data);
-                    });
-                    json = string.slice(0, index + 1);
-                    try {
-                        result = JSON.parse(json) || {started: false, error: {message: "Unknown error"}};
-                    } catch (err) {
-                        logger.warn("Worker send error content", response.toString("utf8"));
-                        process.stderr.write(response);
-                        result = {started: false, error: err};
-                    }
-                    response = response.slice((new Buffer(json, "utf8")).length + 1);
-                    if (!result.started) {
-                        error = new CommonError(result.error);
-                        logger.fatal("Something went wrong", error);
-                        callback(error);
-                    } else {
-                        logger.info("Less daemon started");
-                        callback(null);
-                    }
-                }
-            };
-
         deferred([
             (next:() => void):void => {
-                command = cp.spawn(process.execPath, [this.getDaemon(), "--location", this.getLocation()]);
+                var command:cp.ChildProcess = cp.spawn(process.execPath, [this.getDaemon(), "--location", this.getLocation()]),
+                    data:Buffer = new Buffer(0),
+                    echo:(stream:NodeJS.WritableStream, data:Buffer) => void =
+                        (stream:NodeJS.WritableStream, data:Buffer):void => {
+                            stream.write(data);
+                        },
+                    handler:(buffer:Buffer) => void = (buffer:Buffer):void => {
+                        var result:any,
+                            errors:Error[] = [],
+                            string:string,
+                            index:number,
+                            json:string;
+                        data   = Buffer.concat([data, buffer]);
+                        string = buffer.toString("utf8");
+                        index  = string.indexOf("\n");
+                        if (index !== -1) {
+                            command.stderr.removeListener("data", handler);
+                            command.stdout.removeListener("data", handler);
+                            command.stderr.addListener("data", (data:Buffer):void => {
+                                echo(process.stderr, data);
+                            });
+                            command.stdout.addListener("data", (data:Buffer):void => {
+                                echo(process.stdout, data);
+                            });
+                            json = string.slice(0, index + 1);
+                            try {
+                                result = JSON.parse(json) || {started: false, errors: [{message: "Unknown error"}]};
+                            } catch (err) {
+                                logger.warn("Worker send error content", data.toString("utf8"));
+                                process.stderr.write(data);
+                                result = {started: false, errors: [CommonError.convertToObject(err)]};
+                            }
+                            data = data.slice((new Buffer(json, "utf8")).length + 1);
+                            if (!result.started) {
+                                if (typeOf(result.errors) === "array") {
+                                    errors = (<any[]>result.errors).map((item:any):Error => {
+                                        return new CommonError(item);
+                                    });
+                                }
+                                logger.fatal("Something went wrong", errors); // todo: по другому выводить ошибки
+                                callback(errors);
+                            } else {
+                                logger.info("Less daemon started");
+                                next();
+                            }
+                        }
+                    };
                 command.stderr.addListener("data", handler);
                 command.stdout.addListener("data", handler);
             },
             (next:() => void):void => {
-                super.connect((errors?:Error[]):void => {
-                });
+                super.connect(callback);
             }
         ]);
 
