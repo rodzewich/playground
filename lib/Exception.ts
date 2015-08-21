3ded3e33983a21ea007402045744e9491c32b426
rodzewich <rodzewich.oleg@gmail.com>
2015-08-21 19:02:35 +0300

    -

--- lib/Exception.ts
+++ lib/Exception.ts
@@ -1,10 +1,24 @@
-// todo: сделать нормальный stack
+declare class Error {
+
+    public name:string;
+
+    public message:string;
+
+    public stack:string;
+
+    constructor(message?:string);
+
+    static captureStackTrace(error:Error, func:any):void;
+
+}
 
 class Exception extends Error {
 
     public name:string = "Exception";
 
     public message:string;
+
+    public stack:string;
 
     constructor(message?:string) {
         super();
@@ -19,6 +33,10 @@ class Exception extends Error {
 
     public getMessage():string {
         return this.message;
+    }
+
+    public static captureStackTrace(error:Error, func:any):void {
+        Error.captureStackTrace(error, func);
     }
 
 }
