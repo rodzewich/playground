f510077542f560fe55fbe21d7d7e826ad799b7d5
rodzewich <rodzewich.oleg@gmail.com>
2015-09-03 19:37:09 +0300

    -

--- lib/postcss/Compiler.ts
+++ lib/postcss/Compiler.ts
@@ -3,6 +3,8 @@ import IResult = require("./IResult");
 import IOptions = require("./IOptions");
 import IPlugin = require("./plugins/IPlugin");
 import Base = require("./Base");
+import postcss = require("postcss");
+import postcssSafeParser = require("postcss-safe-parser");
 
 class Compiler extends Base implements ICompiler {
 
@@ -11,18 +13,29 @@ class Compiler extends Base implements ICompiler {
     }
 
     protected getPlugins():IPlugin[] {
-        var plugins:IPlugin[] = [];
-        plugins = plugins.concat(this.getFallbacks());
-        return plugins;
-    }
-
-    protected getFallbacks():IPlugin[] {
         return <IPlugin[]>[
             this.getAutoprefixerPlugin()
         ];
     }
 
     compile(source:string, map?:any, callback?:(errors?:Error[], result?:IResult) => void):void {
+        var plugins: any[] = this.getPlugins().map((plugin: IPlugin): void => {
+            return plugin.getInstance();
+        });
+        postcss(this.getPlugins().map((plugin:IPlugin):void => {
+            return plugin.getInstance();
+        })).process(source, {
+            parser: postcssSafeParser,
+            map: {
+                inline: false,
+                prev: options.map || false,
+                sourcesContent: false,
+                annotation: false
+            }
+        }).then((result:any):void => {
+            callback(null, {content: result.css, map: result.map});
+        }).catch((error?:Error): void => {
 
+        });
     }
 }
