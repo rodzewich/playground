952a77b916ef9b0665ff8615c17a6a9034458446
rodzewich <rodzewich.oleg@gmail.com>
2015-09-14 17:39:24 +0300

    -

--- lib/postcss/Warning.ts
+++ lib/postcss/Warning.ts
@@ -1,5 +1,7 @@
-import typeOf = require("../typeOf");
+import isDefined = require("../isDefined");
 import IWarning = require("./IWarning");
+import SourceCodePointCreator = require("../helpers/SourceCodePointCreator");
+import ISourceCodePointCreator = require("../helpers/ISourceCodePointCreator");
 
 class Warning implements IWarning {
 
@@ -9,24 +11,29 @@ class Warning implements IWarning {
 
     private _filename:string;
 
+    private _content:string;
+
     private _line:number;
 
     private _column:number;
 
     constructor(options:{text: string; plugin: string; filename: string; content: string; line: number; column: number;}) {
-        if (options && typeOf(options.text) !== "undefined") {
+        if (options && isDefined(options.text)) {
             this._text = options.text;
         }
-        if (options && typeOf(options.plugin) !== "undefined") {
+        if (options && isDefined(options.plugin)) {
             this._plugin = options.plugin;
         }
-        if (options && typeOf(options.filename) !== "undefined") {
+        if (options && isDefined(options.filename)) {
             this._filename = options.filename;
         }
-        if (options && typeOf(options.line) !== "undefined") {
+        if (options && isDefined(options.content)) {
+            this._content = options.content;
+        }
+        if (options && isDefined(options.line)) {
             this._line = options.line;
         }
-        if (options && typeOf(options.column) !== "undefined") {
+        if (options && isDefined(options.column)) {
             this._column = options.column;
         }
     }
@@ -43,6 +50,10 @@ class Warning implements IWarning {
         return this._filename;
     }
 
+    public getContent():string {
+        return this._content;
+    }
+
     public getLine():number {
         return this._line;
     }
@@ -52,11 +63,14 @@ class Warning implements IWarning {
     }
 
     public toString():string {
-        var message:string = ["Warning:", this.getText(),
-            "in", [JSON.stringify(this.getFilename()), "(plugin: ", JSON.stringify(this.getPlugin()), ")"].join(""),
-            "on line", String(this.getLine()),
-            ", column", String(this.getColumn())
-        ].join(" ");
+        var creator:ISourceCodePointCreator = new SourceCodePointCreator(), // todo: use parameters
+            message:string = [
+                "Warning:", this.getText(),
+                "in", [this.getFilename(), "(", String(this.getLine()), ":", String(this.getColumn()), ")"].join(""),
+                "from", String(this.getPlugin())
+            ].join(" "),
+            code:string = creator.create(this.getContent(), this.getLine(), this.getColumn());
+        return [message, code].join("\n");
     }
 
 }
