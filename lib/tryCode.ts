a9c5f94f1366c4600b329eecd675afd5122cff31
rodzewich <rodzewich.oleg@gmail.com>
2015-09-16 10:56:48 +0300

    -

---
+++ lib/tryCode.ts
@@ -0,0 +1,34 @@
+import typeOf = require("./typeOf");
+
+class TryCode {
+
+    private _error:Error = null;
+
+    private _catched:boolean = false;
+
+    constructor(callback:() => void) {
+        try {
+            if (typeOf(callback) === "function") {
+                callback();
+            }
+        } catch (error) {
+            this._error = <Error>error;
+        }
+    }
+
+    public catchException(type:any, callback:(error:Error) => void):TryCode {
+        if (!this._catched && this._error instanceof type) {
+            this._catched = true;
+            callback(this._error);
+        }
+        return this;
+    }
+
+}
+
+
+function tryCode(callback:() => void) {
+    return new TryCode(callback);
+}
+
+export = tryCode;
