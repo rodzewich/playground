716efdc00000409d9678711220f9374b655c9c1c
rodzewich <rodzewich.oleg@gmail.com>
2015-09-30 11:36:03 +0300

    -

--- xlib/ui/element/example.ts
+++ xlib/ui/element/example.ts
@@ -1,54 +1,102 @@
-module xlib.ui.element {
+import factory = require("../factory");
+import ITagsList = require("../ITagsList");
 
-  setTimeout(() => {
+interface IMap {
+    canvas?:IDivElement;
+    tbody?:ITBodyElement;
+    tfoot?:ITFootElement;
+    botton?:IBottonElement;
+}
 
-    element.ready(() => {
+factory((tags:ITagsList):void => {
 
-      var component = element.table({
-        id: "element-123",
-        classes: ["xlib-element", "user-factory"],
+    var map:IMap = {};
+
+    tags.div({
+        styles : {
+            border : "2px solid red",
+            fontWeight : "bold"
+        },
+        classes : [
+            "xlib-bold",
+            "xlib-italic"
+        ],
+        name : "canvas",
+        map : map,
         items: [
-          element.thead({
-            classes: ["xlib-head", "xlib-inner"],
-            items: [
-              element.tr({items: [
-                element.th({
-                  styles: {
-                    borderRadius: "11px",
-                    color: "red"
-                  },
-                  text: "text\n<strong>1</strong>"
-                }),
-                element.th({
-                  text: "text<strong>2</strong>"
-                }),
-                element.th({
-                  html: "text<strong>3</strong>"
-                })
-              ]})
-            ]
-          }),
-          element.tbody({
-            classes: ["xlib-body", "xlib-inner"]
-          }),
-          element.tfoot({
-            classes: ["xlib-foot", "xlib-inner"],
-            items: [
-              element.tr({items: [
-                element.td({
-                  text: "button"
-                })
-              ]})
-            ]
-          })
+            tags.table({
+                items : [
+                    tags.caption({
+                        text: lang("PROFILE.TABLE_NAME", "Name")
+                    }),
+                    tags.thead({
+                        items: [
+                            tags.tr({
+                                items: [
+                                    tags.th({
+                                        text: lang("PROFILE.USER_NAME", "User")
+                                    }),
+                                    tags.th({
+                                        text: lang("PROFILE.RESULT")
+                                    })
+                                ]
+                            })
+                        ]
+                    }),
+                    tags.tbody({
+                        items: [
+                            tags.tr({
+                                items: [
+                                    tags.td({
+                                        text: "sfdfsdfs"
+                                    }),
+                                    tags.td({
+                                        text: "sfdfsdfs"
+                                    })
+                                ]
+                            })
+                        ]
+                    }),
+                    tags.tfoot({
+                        items: [
+                            tags.tr({
+                                items: [
+                                    tags.td({
+                                        colspan: 2,
+                                        items: [
+                                            tags.botton({
+                                                text: lang("COMMON.APPLY")
+                                            })
+                                        ]
+                                    })
+                                ]
+                            })
+                        ]
+                    })
+                ]
+            }),
+            tags.button({
+                name : "button",
+                text : lang("COMMON.UPGRADE"),
+                listeners : {
+                    click(event:IClickEvent): void {
+
+                    },
+                    dblClick(event:IDblClickEvent): void {
+
+                    },
+                    mouseOver(event:IMouseOverEvent): void {
+
+                    }
+                }
+            })
         ]
-
-      });
-
-      console.log(component.content());
-
     });
 
-  }, 0);
+    if (options.allowLocalHandlers) {
+        map.botton.onClick((event:IClickEvent):void => {
+            map.canvas.hide();
+        });
+    }
 
-}
+});
