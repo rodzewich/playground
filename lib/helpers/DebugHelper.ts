232e65fd3d7cc45c16da71cfb74c16bb12d1e919
rodzewich <rodzewich.oleg@gmail.com>
2015-10-14 19:02:29 +0300

    -

---
+++ lib/helpers/DebugHelper.ts
@@ -0,0 +1,16 @@
+import IDebugHelper = require("./IDebugHelper");
+
+class DebugHelper implements IDebugHelper {
+
+    public isDebug():boolean {
+    }
+
+    public getIsDebug():boolean {
+    }
+
+    public setIsDebug(value:boolean):void {
+    }
+
+}
+
+export = DebugHelper;
---
+++ lib/helpers/IDebugHelper.ts
@@ -0,0 +1,7 @@
+interface IDebugHelper {
+    isDebug():boolean;
+    getIsDebug():boolean;
+    setIsDebug(value:boolean):void;
+}
+
+export = IDebugHelper;
