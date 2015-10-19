65bdb1367c5a7685e0b012c14c67b2b80a6744e4
rodzewich <rodzewich.oleg@gmail.com>
2015-10-19 13:30:24 +0300

    -

--- tests.ts
+++ tests.ts
@@ -1,28 +1,38 @@
 /// <reference path="./types/glob/glob.d.ts" />
+/// <reference path="./types/optimist/optimist.d.ts" />
 
 import glob      = require("glob");
+import optimist  = require("optimist");
+import isTrue    = require("./lib/isTrue");
 import deferred  = require("./lib/deferred");
 import Exception = require("./lib/exception/Exception");
 import displayException = require("./lib/displayException");
 
 require("./lib/mapping");
 
+var argv:any = optimist
+    .usage("Usage: tests [--debug] --test ./path/to/test.js")
+    .boolean("l").alias("d", "debug").describe("d", "Debug sign")
+    .demand("t").alias("t", "test").describe("m", "Path to test")
+    .argv;
+
 process.addListener('uncaughtException', function (error:Error) {
     displayException(Exception.convertFromError(error));
 });
 
-var debug:boolean = true;
-
-glob("./lib/**/tests/*.js", (errors, files):void => {
-    var actions:((next:() => void) => void)[] = [];
-    files.forEach((file:string):void => {
-        actions.push((next:() => void):void => {
-            console.log("Running:", file);
-            require(file)(debug, ():void => {
-                next();
+if (argv.test) {
+    require(argv.test)(isTrue(argv.debug), ():void => {});
+} else {
+    glob("./lib/**/tests/*.js", (errors, files):void => {
+        var actions:((next:() => void) => void)[] = [];
+        files.forEach((file:string):void => {
+            actions.push((next:() => void):void => {
+                console.log("Running:", file);
+                require(file)(isTrue(argv.debug), ():void => {
+                    next();
+                });
             });
         });
+        deferred(actions);
     });
-    deferred(actions);
-});
-
+}
