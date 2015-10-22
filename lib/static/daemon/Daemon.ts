f5a3312fb1c9f580955113935bc62d1a77ad2465
rodzewich <rodzewich.oleg@gmail.com>
2015-10-22 18:47:48 +0300

    -

--- lib/static/daemon/Daemon.ts
+++ lib/static/daemon/Daemon.ts
@@ -5,6 +5,69 @@ import log4js     = require("../../../logger");
 var logger:log4js.Logger = log4js.getLogger("memory");
 
 class Daemon extends DaemonBase implements IDaemon {
+
+    public getContent(filename:string):void {
+
+    }
+
+    protected handler(request:any, callback:(response:any) => void):void {
+
+        function handler(response:any):void {
+            if (isFunction(callback)) {
+                setTimeout((): void => {
+                    callback(response);
+                }, 0).ref();
+            }
+        }
+
+        super.handler(request, (response:any) => {
+
+            var args:any[]     = request.args || [],
+                command:string = <string>args.shift(),
+                error:IException;
+
+            try {
+
+                switch (command) {
+                    case "ping":
+                        response.result = null;
+                        handler(response);
+                        break;
+                    case "stop":
+                        response.result = null;
+                        handler(response);
+                        this.stop();
+                        break;
+                    case "getContent":
+                        this.getContent(<string>args[0], <IRequest>args[1], (errors:Exception[], result:IResponse):void => {
+                            response.result = result;
+                            if (errors && errors.length) {
+                                response.errors = errors.map((error:Exception):IObject => {
+                                    return error.toObject();
+                                });
+                            }
+                            handler(response);
+                        });
+                        break;
+                    default:
+                        error = new Exception({message: "unknown command"});
+                        response.errors = [error.toObject()];
+                        logger.error(error.getStack());
+                        handler(response);
+                        break;
+                }
+
+            } catch (error) {
+
+                response.errors = ExceptionBase.convertFromError(error).toObject();
+                logger.error(ExceptionBase.convertFromError(error).getStack());
+                handler(response);
+
+            }
+
+        });
+    }
+
 }
 
 export = Daemon;
