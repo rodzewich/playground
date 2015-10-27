14ecc55528b3a9d7866c490d36896abf4755f506
rodzewich <rodzewich.oleg@gmail.com>
2015-10-27 11:06:30 +0300

    -

--- config.ts
+++ config.ts
@@ -17,18 +17,18 @@ module config {
     export const PROJECT_DIRECTORY:string = process.cwd();
     export const PROJECT_CONFIG:string = path.join(PROJECT_DIRECTORY, "config.json");
 
-    export const DEFAULT_TEMPORARY_DIRECTORY:string = path.join(PROJECT_DIRECTORY, "temp");
-    export const DEFAULT_PUBLIC_DIRECTORY:string = path.join(PROJECT_DIRECTORY, "public");
-    export const DEFAULT_MEMORY_SOCKET:string = path.join(DEFAULT_TEMPORARY_DIRECTORY, "memory.sock");
-    export const DEFAULT_CSS_SOCKET:string = path.join(DEFAULT_TEMPORARY_DIRECTORY, "css.sock");
-    export const DEFAULT_LESS_SOCKET:string = path.join(DEFAULT_TEMPORARY_DIRECTORY, "less.sock");
-    export const DEFAULT_SASS_SOCKET:string = path.join(DEFAULT_TEMPORARY_DIRECTORY, "sass.sock");
-    export const DEFAULT_STYLUS_SOCKET:string = path.join(DEFAULT_TEMPORARY_DIRECTORY, "stylus.sock");
+    export const DEFAULT_PROJECT_TEMPORARY_DIRECTORY:string = path.join(PROJECT_DIRECTORY, "temp");
+    export const DEFAULT_PROJECT_PUBLIC_DIRECTORY:string = path.join(PROJECT_DIRECTORY, "public");
+    export const DEFAULT_PROJECT_MEMORY_SOCKET:string = path.join(DEFAULT_PROJECT_TEMPORARY_DIRECTORY, "memory.sock");
+    export const DEFAULT_CSS_SOCKET:string = path.join(DEFAULT_PROJECT_TEMPORARY_DIRECTORY, "css.sock");
+    export const DEFAULT_LESS_SOCKET:string = path.join(DEFAULT_PROJECT_TEMPORARY_DIRECTORY, "less.sock");
+    export const DEFAULT_SASS_SOCKET:string = path.join(DEFAULT_PROJECT_TEMPORARY_DIRECTORY, "sass.sock");
+    export const DEFAULT_STYLUS_SOCKET:string = path.join(DEFAULT_PROJECT_TEMPORARY_DIRECTORY, "stylus.sock");
     export const DEFAULT_NAMESPACE_SEPARATOR:Separator = Separator.DOT;
-    export const DEFAULT_STATIC_MEMORY_METADATA:string = DEFAULT_MEMORY_SOCKET;
-    export const DEFAULT_STATIC_MEMORY_BINARY:string = DEFAULT_MEMORY_SOCKET;
-    export const DEFAULT_STATIC_MEMORY_GZIP:string = DEFAULT_MEMORY_SOCKET;
-    export const DEFAULT_STATIC_MEMORY_LOCK:string = DEFAULT_MEMORY_SOCKET;
+    export const DEFAULT_STATIC_MEMORY_METADATA:string = DEFAULT_PROJECT_MEMORY_SOCKET;
+    export const DEFAULT_STATIC_MEMORY_BINARY:string = DEFAULT_PROJECT_MEMORY_SOCKET;
+    export const DEFAULT_STATIC_MEMORY_GZIP:string = DEFAULT_PROJECT_MEMORY_SOCKET;
+    export const DEFAULT_STATIC_MEMORY_LOCK:string = DEFAULT_PROJECT_MEMORY_SOCKET;
     export const DEFAULT_STATIC_MEMORY_NAMESPACE:INamespaceHelper = new NamespaceHelper(["static"], DEFAULT_NAMESPACE_SEPARATOR);
     export const DEFAULT_STATIC_METADATA_MEMORY_NAMESPACE:string = NamespaceHelper.parse(DEFAULT_STATIC_MEMORY_NAMESPACE.getValue(), DEFAULT_NAMESPACE_SEPARATOR).addToNamespace(["metadata"]);
     export const DEFAULT_STATIC_BINARY_MEMORY_NAMESPACE:string = NamespaceHelper.parse(DEFAULT_STATIC_MEMORY_NAMESPACE.getValue(), DEFAULT_NAMESPACE_SEPARATOR).addToNamespace(["binary"]);
@@ -48,7 +48,7 @@ module config {
 
     export function getTemporaryDirectory():string {
         var config:any = getConfig(),
-            directory:string = DEFAULT_TEMPORARY_DIRECTORY;
+            directory:string = DEFAULT_PROJECT_TEMPORARY_DIRECTORY;
         if (config && isString(config.TEMPORARY_DIRECTORY)) {
             directory = config.TEMPORARY_DIRECTORY;
         }
@@ -60,7 +60,7 @@ module config {
 
     export function getPublicDirectory():string {
         var config:any = getConfig(),
-            directory:string = DEFAULT_PUBLIC_DIRECTORY;
+            directory:string = DEFAULT_PROJECT_PUBLIC_DIRECTORY;
         if (config && isString(config.PUBLIC_DIRECTORY)) {
             directory = config.PUBLIC_DIRECTORY;
         }
